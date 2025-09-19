3a67e8c212d6e0fc15d2d70e15e8e75e8a53f46a
BoomCheeseBurge <brandonjohnlyc@gmail.com>
2025-09-19 14:20:45 +0700

    Nativewind Setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,16 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 items-center justify-center bg-white"
     >
-        <Link href="/sign-in">Sign In</Link>
+      	<Text className="font-bold text-lg my-10">Welcome to the Real Estate App</Text>
+		<Text className="text-xl font-bold text-blue-500">
+			Welcome to Nativewind!
+		</Text>
+		<Link href="/sign-in">Sign In</Link>
         <Link href="/explore">Explore</Link>
         <Link href="/profile">Profile</Link>
         <Link href="/properties">Properties</Link>
