a18c2c2e4553cdca0d8a53b1db387dd3e14185bd
BoomCheeseBurge <brandonjohnlyc@gmail.com>
2025-11-02 22:59:24 +0700

    Implement Home Screen

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,75 @@
-import { Text, View } from "react-native";
+import { FeaturedCard, RegularCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
-    <View
-      className="flex-1 items-center justify-center bg-white"
-    >
-      	<Text className="font-bold font-rubik text-3xl my-10">Welcome to ReState</Text>
+return (
+    <SafeAreaView className="bg-white h-full">
+        
+        <View className="px-5">
+            {/* HEADER Section */}
+            <View className="flex flex-row items-center justify-between mt-5">
+                <View className="flex flex-row items-center">
+                    <Image source={images.avatar} className="size-12 rounded-full"/>
+                    
+                    <View className="flex flex-col items-start ml-2 justify-center">
+                        <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
 
-		<Text className="text-xl font-bold text-blue-500">
-			Powered by Nativewind and Expo Go
-		</Text>
-    </View>
-  );
+                        <Text className="text-base font-rubik-medium text-black-300">John Doe</Text>
+                    </View>
+                </View>
+
+                <Image source={icons.bell} className="size-6"/>
+            </View>
+
+            {/* SEARCH Section */}
+            <Search />
+
+            {/* FEATURED Section */}
+            <View className="my-5">
+                <View className="flex flex-row items-center justify-between">
+                    <Text className="text-xl font-rubik-bold text-black-300">
+                        Featured
+                    </Text>
+
+                    <TouchableOpacity>
+                        <Text className="text-base font-rubik-bold text-primary-300">
+                            See All
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View className="flex flex-row gap-5 mt-5">
+                    <FeaturedCard />
+                    <FeaturedCard />
+                    <FeaturedCard />
+                </View>
+            </View>
+
+            {/* RECOMMENDATIONS Section */}
+            <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                    Our Recommendations
+                </Text>
+
+                <TouchableOpacity>
+                    <Text className="text-base font-rubik-bold text-primary-300">
+                        See All
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+            <Filters />
+
+            <View className="flex flex-row gap-5 mt-5">
+                <RegularCard />
+                <RegularCard />
+            </View>
+        </View>
+    </SafeAreaView>
+);
 }
---
+++ components/Search.tsx
@@ -0,0 +1,47 @@
+import icons from '@/constants/icons';
+import { router, useLocalSearchParams, usePathname } from 'expo-router';
+import React, { useState } from 'react';
+import { Image, TextInput, TouchableOpacity, View } from 'react-native';
+import { useDebouncedCallback } from 'use-debounce';
+
+const Search = () => {
+
+    // Get the imaginary path of the current screen
+    const path = usePathname();
+    // Get the parameters from the path
+    const params = useLocalSearchParams<{ query?: string }>();
+    // Set the search query
+    const [search, setSearch] = useState(params.query);
+
+    // Debounce the search input
+    const debouncedSearch = useDebouncedCallback((text: string) => router.setParams({ query: text }), 500);
+
+    // Handle search input change
+    const handleSearchChange = (text: string) => {
+        setSearch(text);
+        // Do debounce
+        debouncedSearch(text);
+    }
+
+  return (
+    <View className='flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2'>
+        <View className='flex-1 flex flex-row items-center justify-start z-50'>
+            <Image source={icons.search} className='size-5'/>
+
+            <TextInput 
+                value={search}
+                onChangeText={handleSearchChange}
+                placeholder='Search for anything'
+                className='text-sm font-rubik text-black-300 ml-2 flex-1'
+            />
+        </View>
+
+        {/* Show additional filtering options if necessary */}
+        <TouchableOpacity>
+            <Image source={icons.filter} className='size-5'/>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Search
